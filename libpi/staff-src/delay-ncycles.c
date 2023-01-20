bb1c13dc9d0d28d79c4ee24691dfa855b9399069
engler <ddd.rrr.eee@gmail.com>
2023-01-19 19:14:33 -0800

    minor

---
+++ libpi/staff-src/delay-ncycles.c
@@ -0,0 +1,16 @@
+#include "rpi.h"
+
+// roughly 2-3x the number of cyles.  dunno if we care.  can read cycle count from
+// cp15 iirc.
+void delay_cycles(unsigned ticks) {
+#ifndef RPI_UNIX
+    while(ticks-- > 0)
+        asm("add r1, r1, #0");
+#else
+    void nop(void);
+    while(ticks-- > 0)
+        nop();
+#endif
+}
+
+
